b4987a6fca7ea46125c90ef764c2bf09eb7ab504
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2018-12-20 17:54:47 +0200

    Add something

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    
+    public LayerMask blockingLayer;
+
+    private CapsuleCollider2D capsuleCollider;
+    private Rigidbody2D rb;
+
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+    protected float shellDistance = 1f;
+
+    float speed = 10;
+
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical);
+
+        rb.MovePosition(transform.position + speed * movement * Time.deltaTime);
+
+
+    }
+  
+}

f870cb4b3484794f7f1f9a31bfffdb4719af62df
oleynik.a.o <oleynik.a.o@gmail.com>
2018-12-21 15:59:31 +0200

    Add PathFinding algorithm

---
+++ Assets/Scripts/PathFinding/Moving.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public Transform target;
+    float speed = 20;
+    Vector2[] path;
+    int targetIndex;
+
+
+    void Start()
+    {
+        GetComponent<PathRequestManager>().RequestPath(transform.position, target.position, OnPathFound);
+    }
+
+    public void OnPathFound(Vector2[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector2 currentWaypoint = path[0];
+        while (true)
+        {
+            if ((Vector2)transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], Vector2.one);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PathFinding/PathRequestManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PathRequestManager : MonoBehaviour
+{
+    Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
+    PathRequest currentPathRequest;
+
+    static PathRequestManager instance;
+    PathFinding pathfinding;
+
+    bool isProcessingPath;
+
+    void Awake()
+    {
+        instance = this;
+        pathfinding = GetComponent<PathFinding>();
+    }
+
+    public void RequestPath(Vector2 pathStart, Vector2 pathEnd, Action<Vector2[], bool> callback)
+    {
+        PathRequest newRequest = new PathRequest(pathStart, pathEnd, callback);
+        pathRequestQueue.Enqueue(newRequest);
+        TryProcessNext();
+    }
+
+    void TryProcessNext()
+    {
+        if (!isProcessingPath && pathRequestQueue.Count > 0)
+        {
+            currentPathRequest = pathRequestQueue.Dequeue();
+            isProcessingPath = true;
+            pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+        }
+    }
+
+    public void FinishedProcessingPath(Vector2[] path, bool success)
+    {
+        currentPathRequest.callback(path, success);
+        isProcessingPath = false;
+        TryProcessNext();
+    }
+
+
+    struct PathRequest
+    {
+        public Vector2 pathStart;
+        public Vector2 pathEnd;
+        public Action<Vector2[], bool> callback;
+
+        public PathRequest(Vector3 _start, Vector3 _end, Action<Vector2[], bool> _callback)
+        {
+            pathStart = _start;
+            pathEnd = _end;
+            callback = _callback;
+
+        }
+    }
+}
